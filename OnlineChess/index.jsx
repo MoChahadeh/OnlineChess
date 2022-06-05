563937caf204786d09d87df8a005feeb9a5c7cff
Mo Chahadeh <applednd2002@gmail.com>
2022-06-05 15:35:31 +0300

    initalized board and its media queries

--- OnlineChess/index.jsx
+++ OnlineChess/index.jsx
@@ -3,13 +3,46 @@ const rootElement = document.getElementById("root");
 
 class App extends React.Component {
 
-
     render() {
         return (
-            <h1>Under Construction React!!</h1>
+            <div id="container">
+                <Board />
+            </div>
         )
     }
 
+}
+
+const Board = (props) => {
+
+
+
+    return(
+        <div id="board">
+            {
+                new Array(8).fill(null).map((a,rowIndex) => {
+                    return <div className="row" id={`row${rowIndex}`} style={{marginTop : rowIndex == 0? 0 : -2}}>
+                        {
+                            new Array(8).fill(null).map((b, colIndex) => {
+                                return <Square id={`${rowIndex}${colIndex}`} />
+                            })
+                        }
+                    </div>
+                })
+            }
+        </div>
+    );
+
+}
+
+
+const Square = (props) => {
+
+    return (
+        <div className="square" id={props.id}>
+            <p>{props.id}</p>
+        </div>
+    );
 }
 
 
