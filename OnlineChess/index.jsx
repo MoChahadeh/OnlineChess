5cc4038f8b70ab347133301812217d218486b72b
Mo Chahadeh <applednd2002@gmail.com>
2022-06-05 14:45:23 +0300

    Initialized Online Chess React Interface

---
+++ OnlineChess/index.jsx
@@ -0,0 +1,17 @@
+const rootElement = document.getElementById("root");
+
+
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <h1>Under Construction React!!</h1>
+        )
+    }
+
+}
+
+
+
+ReactDOM.render( <App />,rootElement)
